ed4b19fe8759da4c0064a0bd0785cb7ed600fa74
João <oliveirapedro.joao2301@gmail.com>
2026-02-20 18:13:30 +0000

    add method register to AdminService

--- src/main/java/com/joaooliveira/gymcontrol/service/AdminService.java
+++ src/main/java/com/joaooliveira/gymcontrol/service/AdminService.java
@@ -1,5 +1,7 @@
 package com.joaooliveira.gymcontrol.service;
 
+import com.joaooliveira.gymcontrol.model.Admin;
+import com.joaooliveira.gymcontrol.model.Admin.StatusAdmin;
 import com.joaooliveira.gymcontrol.repository.AdminRepository;
 
 public class AdminService {
@@ -10,6 +12,22 @@ public class AdminService {
     }
     
     public void registerAdmin(String username, String password){
-        if(username == null || username.isBlank())
+        if(username == null || username.isBlank()){
+            throw new IllegalArgumentException("Username is invalid");
+        }
+        
+        if(repository.existsByUsername(username)){
+            throw new IllegalArgumentException("Username already exists");
+        }
+        
+        if(password == null || password.isBlank() || password.length() < 8){
+            throw new IllegalArgumentException("Password is invalid");
+        }
+        
+        StatusAdmin status = StatusAdmin.ACTIVE;
+        
+        Admin admin = new Admin(username, password, status);
+        
+        repository.addAdmin(admin);
     }
 }
